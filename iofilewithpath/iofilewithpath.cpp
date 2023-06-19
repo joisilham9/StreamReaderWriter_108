634bce26372de19a53676481ba99113637cdea03
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-06-19 11:00:09 +0700

    int main

---
+++ iofilewithpath/iofilewithpath.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
+	// membuka file dalam mode menulis.
+	ofstream outfile;
+	// menujuk ke sebuah nama file
+	outfile.open(NamaFile, ios::out);
+
+	cout << ">= Menulis file, \'q'\ untuk keluar " << endl;
+
+	// membuka file dalam mode membaca
+	ifstream infile;
+	// menunjukan ke sebuah file
+	infile.open(NamaFile, ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+
+	}
+}
