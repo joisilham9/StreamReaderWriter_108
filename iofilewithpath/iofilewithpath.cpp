a72d9feec61bfc0d6dbc4e3e5d4e19c88d26acfc
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-06-19 11:28:44 +0700

    melakukan perulangan setiap baris

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -26,6 +26,25 @@ int main() {
 	//jika file ada maka
 	if (infile.is_open())
 	{
+		//membuka file dalam mode file
+		infile.open(NamaFile, ios::in);
 
+		cout << endl << ">= Membuka dan membaca file " << endl;
+		//jika file ada maka
+		if (infile.is_open())
+		{
+
+			// melakukan perulangan setiap baris
+			while (getline(infile, baris))
+			{
+				// dan tampilkan di sini
+				cout << baris << '\n';
+			}
+			//tutup file tersebut setelah selesai
+			infile.close();
+		}
+		// jika tidak ditemukan file makna akan menampilkan ini
+		else cout << " unable to open file";
+		return 0;
 	}
 }
