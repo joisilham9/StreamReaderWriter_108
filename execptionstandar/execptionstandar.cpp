2c1917be48714d6dcd8db97161f05ec9b5b9f2c3
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-06-19 11:30:43 +0700

    int main awal program

---
+++ execptionstandar/execptionstandar.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <exception>
+//
+#include <array>
+using namespace std;
+
+int main()
+{
+	cout << " awal program " << endl; // penanda 1:..
+	try {
+		array <int, 3> data = { 1,2,3 };
+		//pesan array integer 3 elemen
+		// cout << data.at(5)<< endl;
+		// memanggil array elemen ke 5
+
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception 
+		cout << e.what() << endl;
+		/* akana dieksekusi karna array dat hanya memiliki 3 elemen */
+	}
+	cout << "baris program yang terakhir" << endl;
+	/*penanda 2: bahwa program berjalan tenpa berhenti meskipun terjadi kesalahan*/
+	return 0;
+}
