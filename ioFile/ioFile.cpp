96c219f8671f269e4a587c18fce39c1b7f0ac36c
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-06-19 10:21:06 +0700

    Add project files.

---
+++ ioFile/ioFile.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main() {
+	string baris;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menujuk ke sebuah nama file.
+	outfile.open("contohfile.txt");
+
+
+}
