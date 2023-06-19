458983eb42449a0126ac7235a0f7faf984392a3c
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-06-19 10:34:14 +0700

    membuat method getline dan outfile

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -11,4 +11,16 @@ int main() {
 	outfile.open("contohfile.txt");
 
 	cout << ">=Menulis file, \'q'\ untuk keluar" << endl;
+
+	while (true) {
+		cout << "-";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q")break;
+		//menulis dan memasukan nilai  dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	//selesai dalam menulis sekarnag tutup filenya
+	outfile.close();
 }
